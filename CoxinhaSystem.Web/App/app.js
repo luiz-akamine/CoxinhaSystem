78ea80c0ab43a134cc342f06c9644684e07d54b7
luiz-akamine <luiz.akamine@gmail.com>
2016-08-14 22:42:31 -0300

    Criação Esqueleto Front-End

---
+++ CoxinhaSystem.Web/App/app.js
@@ -0,0 +1,39 @@
+﻿//Arquivo raíz de configuração da aplicação
+var app = angular.module('CoxinhaApp', ['ngRoute', 'angular-loading-bar']);
+
+//Configurações das rotas da aplicação
+app.config(function ($routeProvider, $httpProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
+    $httpProvider.defaults.headers.delete = {};
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+    
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+//Constantes da aplicação
+app.constant('ngCoxinhaSettings', {
+    apiServiceBaseUri: 'http://localhost:3001/'
+});
+
+/* TODO
+//Setando serviço que realizará a interceptação das requisições http
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+//Chamando rotina inicial da aplicação
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+*/
