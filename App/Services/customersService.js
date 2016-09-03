885382f85ade94bb4f6de078cc53dc6a82307612
luiz-akamine <luiz.akamine@gmail.com>
2016-09-03 00:56:51 -0300

    Novas implementações sub-tela confirmação pedido
    Inclusão de novos campos para confirmação do Pedido
    Em andamento...

---
+++ App/Services/customersService.js
@@ -0,0 +1,29 @@
+﻿//Serviço com métodos relacionados a entidade de Pedidos
+'use strict';
+app.factory('customersService', ['$http', 'ngCoxinhaSettings', function ($http, ngCoxinhaSettings) {
+
+    //Server onde está hospedado as WEB APIs
+    var serviceBase = ngCoxinhaSettings.apiServiceBaseUri;
+
+    //Variável para acessarmos este serviço
+    var customersServiceFactory = {};
+
+    //Método que cliente a partir de seu telefone
+    var _getByPhoneComplete = function (phoneNumber) {
+
+        //Chamando WEB API no server que retorna a lista de Pedidos        
+        return $http.get(serviceBase + 'api/customer/GetByPhoneComplete?phoneNumber='+ phoneNumber)
+            .success(function (result) {
+                return result;
+            })
+            .error(function (response, status) {
+                console.log('erro ao adquirir cliente. Status: ' + status);
+                alert('erro ao adquirir cliente');
+            });
+    };
+
+    //Definindo métodos desta factory a serem chamadas por outros js
+    customersServiceFactory.getByPhoneComplete = _getByPhoneComplete;
+
+    return customersServiceFactory;
+}]);
--- App/myDirectives.js
+++ App/myDirectives.js
@@ -16,4 +16,146 @@ angular.module('myDirectives', [])
             this.orderjson = directive.$viewValue;
         }*/
     };
+})
+.directive('uiDate', function () {
+    return {
+        restrict: "A",
+        //scope: {},
+        require: "ngModel",
+        link: function (scope, element, attrs, ctrl) {
+            var _formatDate = function (date) {
+                //retirando carcteres diferentes de números
+                date = date.replace(/[^0-9]+/g, "");
+
+                //Inserindo máscara
+                if (date.length >= 2) {
+                    date = date.substring(0, 2) + "/" + date.substring(2);
+                }
+                if (date.length >= 5) {
+                    date = date.substring(0, 5) + "/" + date.substring(5, 9);
+                }
+
+                return date;
+            };
+
+            //atribuindo evento keyup para ao digitarmos, a mascara entrar em ação
+            element.bind("keyup", function () {
+                ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+                ctrl.$render();
+            });
+
+            //Atribuindo ao ngModel apenas quando atingir o número de caracteres completo de data
+            ctrl.$parsers.push(function (value) {
+                if (value.length === 10) {
+                    
+                    //validação da data
+                    if (!moment(value, 'DD/MM/YYYY').isValid()) {
+                        //Limpando variável
+                        ctrl.$setViewValue('');
+                        ctrl.$render();
+
+                        //msg de erro
+                        scope.error = 'Data Inválida';
+
+                        return null;
+                    }
+
+                    var dateArray = value.split("/");
+
+                    return new Date(dateArray[2], dateArray[1] - 1, dateArray[0]);
+                }
+            });
+        }
+    };
+})
+.directive('uiTime', function () {
+    return {
+        restrict: "A",
+        //scope: {},
+        require: "ngModel",
+        link: function (scope, element, attrs, ctrl) {
+            var _formatTime = function (time) {
+                if (time != null) {                    
+                    //retirando carcteres diferentes de números
+                    time = time.replace(/[^0-9]+/g, "");
+
+                    //Inserindo máscara
+                    if (time.length >= 2) {
+                        time = time.substring(0, 2) + ":" + time.substring(2, 4);
+                    }
+
+                    return time;
+                }
+            };
+
+            //atribuindo evento keyup para ao digitarmos, a mascara entrar em ação
+            element.bind("keyup", function () {
+                ctrl.$setViewValue(_formatTime(ctrl.$viewValue));
+                ctrl.$render();
+            });
+
+            //Atribuindo ao ngModel apenas quando atingir o número de caracteres completo de data
+            ctrl.$parsers.push(function (value) {
+                if (value.length === 5) {
+
+                    //validação da hora
+                    if (!moment(value, "HH:mm").isValid()) {
+                        ctrl.$setViewValue('');
+                        ctrl.$render();
+
+                        //msg de erro
+                        scope.error = 'Horário Inválido';
+
+                        return null;
+                    };
+
+                    var timeArray = value.split(":");
+
+                    return new Date(0, 0, 0, timeArray[0], timeArray[1]);
+                }
+            });
+        }
+    };
+})
+.directive('uiPhoneBr', function () {
+    return {
+        restrict: "A",
+        scope: {},
+        require: "ngModel",
+        link: function (scope, element, attrs, ctrl) {
+            var _formatPhone = function (phone) {
+                //retirando carcteres diferentes de números
+                phone = phone.replace(/[^0-9]+/g, "");
+
+                //Inserindo máscara
+                if (phone.length >= 1) {
+                    phone = "(" + phone.substring(0, 1) + phone.substring(1);
+                }
+                if (phone.length >= 3) {
+                    phone = phone.substring(0, 3) + ") " + phone.substring(3);
+                }
+                if (phone.length >= 9) {
+                    phone = phone.substring(0, 9) + "-" + phone.substring(9, 14);
+                }
+                if (phone.length >= 15) {
+                    phone = phone.substring(0, 9) + phone[10] + "-" + phone.substring(11, 15);
+                }
+
+                return phone;
+            };
+
+            //atribuindo evento keyup para ao digitarmos, a mascara entrar em ação
+            element.bind("keyup", function () {
+                ctrl.$setViewValue(_formatPhone(ctrl.$viewValue));
+                ctrl.$render();
+            });
+
+            //Atribuindo ao ngModel apenas quando atingir o número de caracteres completo de data
+            ctrl.$parsers.push(function (value) {
+                if (value.length >= 14) {
+                    return value
+                }
+            });
+        }
+    };
 });
